3509afd0705ab1826afe37d38084f7d117b934ae
ankitrana1256 <76605774+ankitrana1256@users.noreply.github.com>
2021-04-22 01:25:14 +0530

    debug

--- NoteBook/settings.py
+++ NoteBook/settings.py
@@ -126,8 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/'),
-    os.path.join(BASE_DIR, '/media/'),
+    os.path.join(BASE_DIR, '/static/')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
